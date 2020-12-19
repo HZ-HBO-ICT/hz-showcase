15f3829ce82c7adafa09d3339bdca138155a0349
MDS02 <smit0108@hz.nl>
2020-12-19 10:10:36 +0100

    initial upload

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class RequestController extends Controller
+{
+    public function index(){
+        //Http::get("https://api.airtable.com");
+
+        $response = Http::withHeaders([
+            'Authorization' => 'Bearer ' . env("AIRTABLE_API_KEY"),
+        ])->get('https://api.airtable.com/v0/appsxUWtKODuadN4y/showcase'
+        )->json();
+        dd($response);
+    }
+}
